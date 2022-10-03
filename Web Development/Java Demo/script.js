0b4309f891bb2c6772d77cc2914b407b2bce6d60
aPROTO <alqamascaptaina3@gmail.com>
2022-10-03 23:28:16 +0530

    added variables in JS

---
+++ Web Development/Java Demo/script.js
@@ -0,0 +1,12 @@
+var x = 35 , y = 19;
+x = x % y;
+var a = 10 , b = 5;
+a = a**b;
+console.log("output is", a, x);
+var z = 3 == 3 == '3' !== '3' === '3'; // == doesn't check the difference between the data types but === does that
+if (z){
+    console.log("yep z is true");
+}
+else{
+    console.log("nope z is false");
+}
