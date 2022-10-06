f1ab7de83b9bc74491924376ad3ce38f90b79e85
aPROTO <alqamascaptaina3@gmail.com>
2022-10-06 22:45:23 +0530

    More javascript to understand for-in and for-of

--- Web Development/Java Demo/script.js
+++ Web Development/Java Demo/script.js
@@ -3,10 +3,11 @@ x = x % y;
 var a = 10 , b = 5;
 a = a**b;
 console.log("output is", a, x);
-var z = 3 == 3 == '3' !== '3' === '3'; // == doesn't check the difference between the data types but === does that
+var z = 3 == 3 == '3' !== '3' == '3'; // == doesn't check the difference between the data types but === does that
 if (z){
     console.log("yep z is true");
 }
 else{
     console.log("nope z is false");
 }
+console.log("Hi Kaisan Ba");
