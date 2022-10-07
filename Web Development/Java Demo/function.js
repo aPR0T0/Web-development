1a40186223cf01de07e6eb3aa6975058890c235f
aPROTO <alqamascaptaina3@gmail.com>
2022-10-07 23:00:33 +0530

    function.js added

---
+++ Web Development/Java Demo/function.js
@@ -0,0 +1,7 @@
+function MakeCake(cream, ice, eggs){
+    console.log("Cake will be ready in "
+                + cream*2 + " hours" + " and ingredients are :- "+ ice + " ice cubes " + " and " + eggs + " eggs")
+}
+
+let cream = 2, ice = 2 , eggs = 6;
+MakeCake(cream,ice,eggs);
