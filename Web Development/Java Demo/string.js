8fa044985d73411acacd744ba6dcd3d0324fd5de
aPROTO <alqamascaptaina3@gmail.com>
2022-10-09 06:32:47 +0530

    string with functions added

--- Web Development/Java Demo/string.js
+++ Web Development/Java Demo/string.js
@@ -1,7 +1,13 @@
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+/*-------------------------------------Normal String Declaration----------------------------------- */
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // A string can be declared using the single and double quotes both ways
 // The best way to declare a string
-
-let dream = `Martha exclaimed, "We are one and we can fight anything and win the moment 'we start believing in ourselves'!!"`;
+let string4 = "let's have some fun!!";
+let dream = `Martha exclaimed, "We are one and we can fight anything and win the moment 'we start believing in ourselves'!! so!, ${string4}"`;
 console.log(dream);
 
 // declaring a string using escape to resolve syntax conflicts
@@ -28,3 +34,23 @@ s1 = Number(s1);
 console.log(s1,"with data type " + typeof s1);
 s1 = s1.toString();
 console.log(s1,"with data-type " + typeof s1);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+/*------------------------------------Functions in strings------------------------------------------*/
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var favIceCream = " Butter Scotch";
+let i = 5;
+console.log(`${favIceCream.length, favIceCream[i].toString()} is it's ${i}th letter and the last letter is ${favIceCream[favIceCream.length-1].toString()} we can also get the index using IndexOf function i.e. ${favIceCream.indexOf("But")}`);
+
+// String can also be sliced
+
+let someString = favIceCream.slice(5,10);
+console.log(someString);
+
+// uppercase and lowercase
+
+var someString2 = "This WAs some String";
+console.log(someString2.toUpperCase());
+console.log(someString2.toLowerCase());
+
